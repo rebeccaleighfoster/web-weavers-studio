5b41ac190b9d45cbe07305464d8b628016006dad
rebeccaleighfoster <rebecca.leigh.foster@gmail.com>
2020-04-22 15:41:53 -0500

    post

--- src/CreateProfile.js
+++ src/CreateProfile.js
@@ -1,110 +1,216 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Nav from "./Nav";
-import './CreateProfile.css'
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Nav from "./Nav"
+import "./CreateProfile.css"
 
-export default class CreateProfile extends React.Component {
-    render() {
+
+const CreateProfileForm = () => (
+  <div>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        experienceLevel: "",
+        loomType: "",
+        State: "",
+        bio: ""
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        fetch(`http://localhost:4000/weavers/`, {
+          method: 'POST',
+          header: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(values)
+        }).then(response => response.json())
+          .then(data => { console.log(data) })
+        // setTimeout(() => {
+        //   alert(JSON.stringify(values, null, 2));
+        //   setSubmitting(false);
+        // }, 500);
+        console.log(values);
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email()
+          .required("Required")
+      })}
+    >
+      {props => {
+        const {
+          values,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          handleReset
+        } = props;
+
         return (
-            <>
-                < Nav />
-                <form id="newWeaver">
-                <h1>New Weaver</h1>
-                    <div>
-                        <label htmlFor="first-name">First name</label>
-                        <input placeholder='First Name' type="text" name='first-name' id='first-name' />
-                    </div>
-                    <div>
-                        <label htmlFor="last-name">Last name</label>
-                        <input type="text" name='last-name' id='last-name' placeholder='Last Name' />
-                    </div>
-                    <div>
-                        <label htmlFor="username">Email</label>
-                        <input type="text" name='username' id='username' />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name='password' id='password' />
-                    </div>
-                    <label htmlFor="experience">Experience Level:</label>
-                    <select id="experience">
-                        <option value="beginner">Beginner</option>
-                        <option value="intermediate">intermediate</option>
-                        <option value="expert">Expert</option>
-                        <option value="master">Master</option>
-                    </select>
-                    <label htmlFor="loom type">Loom Type:</label>
-                    <select id="loom type">
-                        <option value="Rigid heddle">Rigid Heddle</option>
-                        <option value="Tapestry"> Tapestry/Frame </option>
-                        <option value="Table Loom "> Table Loom </option>
-                        <option value="Floor Loom"> Floor Loom </option>
-                    </select>
-                    <label htmlFor="state">Where are you located?</label>
-                    <select id="state">
-                        <option value="AL">Alabama</option>
-                        <option value="AK">Alaska</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="CA">California</option>
-                        <option value="CO">Colorado</option>
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="DC">District Of Columbia</option>
-                        <option value="FL">Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="HI">Hawaii</option>
-                        <option value="ID">Idaho</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IN">Indiana</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NV">Nevada</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="NY">New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="OH">Ohio</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="OR">Oregon</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TN">Tennessee</option>
-                        <option value="TX">Texas</option>
-                        <option value="UT">Utah</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WA">Washington</option>
-                        <option value="WV">West Virginia</option>
-                        <option value="WI">Wisconsin</option>
-                        <option value="WY">Wyoming</option>
-                    </select>
-                    <div className="form-section">
-                        <label htmlFor="project description">Bio</label>
-                        <textarea name="bio" input placeholder='What do you want other weavers to know about you?'></textarea>
-                    </div>
-                    <Link to="./WeaversList">
-                        <button type="submit">Submit</button>
-                    </Link>
-                </form>
-            </>
-        )
-    }
+          <form onSubmit={handleSubmit} className="form">
+            <div>
+              <label htmlFor="firstName">First name</label>
+              <input
+                required
+                value={values.firstName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="First Name"
+                type="text"
+                name="firstName"
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last name</label>
+              <input
+                value={values.lastName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                required
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                required
+                type="text"
+                name="email"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                required
+                type="password"
+                name="password"
+              />
+            </div>
+            <label htmlFor="experience">Experience Level:</label>
+            <select
+              value={values.experience}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+              name="experience"
+            >
+              <option value="beginner">Beginner</option>
+              <option value="intermediate">intermediate</option>
+              <option value="expert">Expert</option>
+              <option value="master">Master</option>
+            </select>
+            <label htmlFor="loomType">Loom Type:</label>
+            <select
+              value={values.loomType}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+              name="loomType"
+            >
+              <option value="Rigid heddle">Rigid Heddle</option>
+              <option value="Tapestry"> Tapestry/Frame </option>
+              <option value="Table Loom "> Table Loom </option>
+              <option value="Floor Loom"> Floor Loom </option>
+            </select>
+            <label htmlFor="location">Where are you located?</label>
+            <select
+              value={values.location}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+              name="location"
+            >
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="DC">District Of Columbia</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
+              <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </select>
+            <button
+              type="button"
+              className="outline"
+              onClick={handleReset}
+              disabled={!dirty || isSubmitting}
+            >
+              Reset
+            </button>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        );
+      }}
+    </Formik>
+  </div>
+);
+
+function CreateProfile() {
+  console.log('create profile ran')
+  return (
+    <>
+      <Nav />
+      <CreateProfileForm />
+    </>
+  )
 }
 
+export default CreateProfile
 //this data will be used to populate the weavers table
--- src/addProject.js
+++ src/addProject.js
@@ -1,55 +1,238 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import Nav from './Nav'
+import { Formik } from 'formik';
+import Nav from './Nav';
+import * as Yup from 'yup';
+import "./addProjects.css"
 
-export default class addProject extends React.Component {
-    render() {
-        return (
-            <>
-                < Nav />
-                <h1>New Project</h1>
-                <form>
-                    <label>
-                        Project Title <input type="text" name="title" placeholder="towels" />
-                    </label>
-                    <label>
-                        Project Description <input type="text" name="description" rows="15" placeholder="description of project" />
-                    </label>
-                    <label>
-                        Weave Structure <input type="text" name="Weave Structure" placeholder="Monks Belt" />
-                    </label>
-                    <label>
-                        Warp Material <input type="text" name="Warp Material" placeholder="Cottolin" />
-                    </label>
-                    <label>
-                        Warp Size <input type="text" name="Warp Size" placeholder="8/4" />
-                    </label>
-                    <label>
-                        Weft Material <input type="text" name="Weft Material" placeholder="Wool" />
-                    </label>
-                    <label>
-                        Weft Size <input type="text" name="Weft Size" placeholder="3/2" />
-                    </label>
-                    <label>
-                        Sett <input type="number" name="sett" placeholder="12" />
-                    </label>
-                    <label>
-                        Picks Per Inch <input type="number" name="ppi" placeholder="12" />
-                    </label>
-                    <label>
-                        Project Width and Length on Loom <input type="text" name="size on loom" placeholder="12in x 22in" />
-                    </label>
-                    <label>
-                        Project Width and Length off Loom <input type="text" name="size off loom" placeholder="10in x 20in" />
-                    </label>
-                </form>
-                <Link to="projectsList">
-                    <button type="submit">Submit</button>
-                </Link>
-            </>
-        )
-    }
+const AddProjectForm = () => (
+    <div>
+        <Formik
+            initialValues={{ project_title: "" }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 500);
+            }}
+            validationSchema={Yup.object().shape({
+                email: Yup.string()
+                    .email()
+                    .required("Required")
+            })}
+        >
+            {props => {
+                const {
+                    values,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                } = props;
+                console.log(values)
+                return (
+                    <form onSubmit={handleSubmit} className="form">
+                        <div>
+                            <label htmlFor="Project_Title">Project Title</label>
+                            <input
+                                id="Project_Title"
+                                name="Project_Title"
+                                type="text"
+                                required
+                                onBlur={handleBlur}
+                                placeholder="Place Mats"
+                                onChange={handleChange}
+                                value={values.project_title}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Project_Description">Project Description</label>
+                            <input
+                                id="Project_Description"
+                                name="Project_Description"
+                                type="text"
+                                required
+                                onBlur={handleBlur}
+                                placeholder="Four placemats and a table runner as a gift for my aunt"
+                                onChange={handleChange}
+                                value={values.project_title}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Weave_Structure">Weave Structure</label>
+                            <input
+                                id="Weave_Structure"
+                                name="Weave_Structure"
+                                type="text"
+                                required
+                                placeholder="Monks Belt"
+                                onChange={handleChange}
+                                value={values.weave_structure}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Warp Material">Warp Material</label>
+                            <input
+                                id="Warp Material"
+                                name="Warp Material"
+                                type="text"
+                                placeholder="Cottolin"
+                                onChange={handleChange}
+                                value={values.Warp_Material}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="weft Material">Weft Material</label>
+                            <input
+                                id="Weft Material"
+                                name="Weft Material"
+                                type="text"
+                                placeholder="Cotton"
+                                onChange={handleChange}
+                                value={values.Weft_Material}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Warp Size">Warp Size</label>
+                            <input
+                                id="Warp Size"
+                                name="Warp Size"
+                                placeholder="3/2"
+                                type="text"
+                                onChange={handleChange}
+                                value={values.Warp_Size}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="WeftSize">Weft Size</label>
+                            <input
+                                id="Weft Size"
+                                name="Weft Size"
+                                type="text"
+                                placeholder="5/2"
+                                onChange={handleChange}
+                                value={values.Weft_Size}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Sett">Sett</label>
+                            <input
+                                id="Sett"
+                                name="Sett"
+                                type="integer"
+                                placeholder="12"
+                                onChange={handleChange}
+                                value={values.Sett}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Sett">Picks Per Inch</label>
+                            <input
+                                id="PPI"
+                                name="PPI"
+                                type="integer"
+                                placeholder="12"
+                                onChange={handleChange}
+                                value={values.PPI}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="size_on_loom">Size On Loom</label>
+                            <input
+                                id="size_on_loom"
+                                name="size_on_loom"
+                                type="text"
+                                placeholder="12in x 14in"
+                                onChange={handleChange}
+                                value={values.size_on_loom}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="size_off_loom">Size Off Loom</label>
+                            <input
+                                id="size_off_loom"
+                                name="size_off_loom"
+                                type="text"
+                                placeholder="12in x 13in"
+                                onChange={handleChange}
+                                value={values.size_off_loom}
+                            />
+                        </div>
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+            </button>
+                    </form>
+                );
+            }}
+        </Formik>
+    </div>
+);
+
+
+
+function AddProject() {
+    console.log('add project ran' )
+    return (
+        <>
+            <Nav />
+            <AddProjectForm />
+        </>
+    )
 }
+
+export default AddProject;
+
 
                 //input form from repl 
                 //this data will be used to populate projects table
+
+
+/*
+export default class addProject extends React.Component {
+render() {
+return (
+<>
+< Nav />
+<h1>New Project</h1>
+<form>
+    <label>
+        Project Title <input type="text" name="title" placeholder="towels" />
+    </label>
+    <label>
+        Project Description <input type="text" name="description" rows="15" placeholder="description of project" />
+    </label>
+    <label>
+        Weave Structure <input type="text" name="Weave Structure" placeholder="Monks Belt" />
+    </label>
+    <label>
+        Warp Material <input type="text" name="Warp Material" placeholder="Cottolin" />
+    </label>
+    <label>
+        Warp Size <input type="text" name="Warp Size" placeholder="8/4" />
+    </label>
+    <label>
+        Weft Material <input type="text" name="Weft Material" placeholder="Wool" />
+    </label>
+    <label>
+        Weft Size <input type="text" name="Weft Size" placeholder="3/2" />
+    </label>
+    <label>
+        Sett <input type="number" name="sett" placeholder="12" />
+    </label>
+    <label>
+        Picks Per Inch <input type="number" name="ppi" placeholder="12" />
+    </label>
+    <label>
+        Project Width and Length on Loom <input type="text" name="size on loom" placeholder="12in x 22in" />
+    </label>
+    <label>
+        Project Width and Length off Loom <input type="text" name="size off loom" placeholder="10in x 20in" />
+    </label>
+</form>
+<Link to="projectsList">
+    <button type="submit">Submit</button>
+</Link>
+</>
+)
+}
+}
+*/
